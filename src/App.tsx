4dd42c39c6d93003ae22578f574b83077a138164
gerbelli27 <guerbell@hotmail.com>
2022-03-13 22:47:18 +0000

    wip mobile

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,57 @@ import iconScissor from './assets/images/iconscissors.svg'
 import iconPaper from './assets/images/iconpaper.svg'
 import iconRock from './assets/images/iconrock.svg'
 import iconLizard from './assets/images/iconlizard.svg'
+import logo from './assets/images/logo-bonus.svg'
 
 
 function App() {
 
 
   return (
-    
-    <div className="container">
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum accusantium pariatur ipsam, unde veritatis maiores quisquam sit illo voluptatem ullam odit repellat quo temporibus molestiae aut dolorum dignissimos illum nemo.</p>
-
-    </div>
-
+    <>
+      <header className="header">
+        <img className="logo" src={logo} alt="Logo" />
+        <div className="score"><h1>SCORE</h1><span>12</span></div>
+      </header>
+      <div className="container">
+        <div className="scissor">
+          <div className="spock-default scissors-color">
+            <div>
+              <img src={iconScissor} alt="Scissor" />
+            </div>
+          </div>
+        </div>
+        <div className="paper">
+          <div className="spock-default paper-color">
+            <div>
+              <img src={iconPaper} alt="Paper" />
+            </div>
+          </div>
+        </div>
+        <div className="rock">
+          <div className="spock-default rock-color">
+            <div>
+              <img src={iconRock} alt="Rock" />
+            </div>
+          </div>
+        </div>
+        <div className="lizard">
+          <div className="spock-default lizard-color">
+            <div>
+              <img src={iconLizard} alt="lizard" />
+            </div>
+          </div>
+        </div>
+        <div className="spock">
+          <div className="spock-default">
+            <div>
+              <img src={iconSpock} alt="Spock" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="rules"><span>RULES</span></footer>
+    </>
   )
 }
 
